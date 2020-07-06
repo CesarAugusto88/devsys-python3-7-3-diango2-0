ee0c69b86e7110528e9fd545326e05e490e2b945
CesarAugusto88 <cesarcosta.augustos@gmail.com>
2020-07-06 13:02:49 -0300

    Colocando imagem nos arquivos para download

--- core/forms.py
+++ core/forms.py
@@ -6,4 +6,4 @@ from .models import Arq
 class ArqForm(forms.ModelForm):
     class Meta:
         model = Arq
-        fields = ('titulo', 'assunto', 'arquivo')
+        fields = ('titulo', 'assunto', 'arquivo', 'imagem')
