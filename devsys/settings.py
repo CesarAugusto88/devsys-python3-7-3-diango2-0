ee2593a0d02ba32191f00df483760b61f8d5b690
CesarAugusto88 <cesarcosta.augustos@gmail.com>
2020-07-13 14:14:10 -0300

    Configurando whitenoise

--- devsys/settings.py
+++ devsys/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 #CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #add whitenoise
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
